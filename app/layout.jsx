9b4187618252d871e066e54efe59bd619c96785e
shoaib <shoaibhossain24@gmail.com>
2023-12-02 10:24:44 +0600

    add structure

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import '@styles/globals.scss';
+
+
+export const matadata = {
+  title: 'ai_prompt',
+  description: 'Discover and share AI generated prompts',
+  
+}
+
+const RootLayout = () => {
+  return (
+   <html lang='en'>
+    <body>
+        <div className='main'>
+            <div className="gradient"/>
+        </div>
+        <main className='app'>
+
+        </main>
+    </body>
+
+   </html>
+  )
+}
+
+export default RootLayout
